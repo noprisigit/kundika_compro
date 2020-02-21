4488fef889fe535054b2b8941f5f4cd8dd26da56
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-02-21 15:07:36 +0700

    menambahkan section partners

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ $('.navbar-nav a').on('click', function () {
     console.log(this);
     $(this).addClass('active').siblings().removeClass('active');
 });
+
+$('.customer-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 4
+        }
+    }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 3
+        }
+    }]
+});
