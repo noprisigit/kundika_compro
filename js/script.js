ca6e1212c07088df5226d284b44ba3fcd8c8392a
Sigit <noprisigit@gmail.com>
2020-04-09 22:35:42 +0700

    update slider

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
-$(window).on('load', function() {
+$(window).on('load', function () {
     $('.jumbotron .display-4').addClass('muncul');
     $('.jumbotron p').addClass('muncul');
 });
 
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
-    
+
     if (wScroll > 80) {
         $('.navbar').css('padding', '0');
     } else {
@@ -14,29 +13,29 @@ $(window).scroll(function () {
     }
 
     $('.jumbotron h1').css({
-        'transform' : 'translate(0px, ' + wScroll/2 +'%)'
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
     });
     $('.jumbotron p').css({
-        'transform' : 'translate(0px, ' + wScroll/1.2 +'%)'
+        'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
     });
 
     if (wScroll > $('.about-us').offset().top - 250) {
         $('.about-us p').addClass('showbox');
-        $('.about-us .box').each(function(i) {
-            setTimeout(function() {
+        $('.about-us .box').each(function (i) {
+            setTimeout(function () {
                 $('.about-us .box').eq(i).addClass('muncul');
-            }, 400 * (i+1));
+            }, 400 * (i + 1));
         });
     } else {
         $('.about-us .box').removeClass('muncul');
         $('.about-us p').removeClass('showbox');
     }
 
-    if(wScroll > $('.services').offset().top - 370) {
-        $('.services .thumbnail').each(function(i) {
-            setTimeout(function() {
-                $('.services .thumbnail').eq(i).addClass('muncul'); 
-            }, 400 * (i+1));
+    if (wScroll > $('.services').offset().top - 370) {
+        $('.services .thumbnail').each(function (i) {
+            setTimeout(function () {
+                $('.services .thumbnail').eq(i).addClass('muncul');
+            }, 400 * (i + 1));
         });
     } else {
         $('.services .thumbnail').removeClass('muncul');
@@ -47,7 +46,7 @@ $(window).scroll(function () {
     //         $('.box-left').addClass('box-muncul');
     //         $('.box-right').addClass('box-muncul');
     //     }, 300);
-        
+
     // } else {
     //     $('.box-left').removeClass('box-muncul');
     //     $('.box-right').removeClass('box-muncul')
@@ -55,13 +54,13 @@ $(window).scroll(function () {
 
     if (wScroll > $('.excellence').offset().top - 300) {
         $('.excellence .box').each(function (i) {
-            setTimeout(function() {
+            setTimeout(function () {
                 if (i % 2 != 0) {
                     $('.box-right').addClass('box-muncul');
                 } else {
                     $('.box-left').addClass('box-muncul');
                 }
-            }, 300 * (i+1));
+            }, 300 * (i + 1));
         });
         // setTimeout(function () {
         //     $('.box-left').addClass('box-muncul');
@@ -75,17 +74,17 @@ $(window).scroll(function () {
 });
 
 $('.customer-logos').slick({
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1000,
-    arrows: false,
+    arrows: true,
     dots: false,
     pauseOnHover: false,
     responsive: [{
         breakpoint: 768,
         settings: {
-            slidesToShow: 3
+            slidesToShow: 2
         }
     }, {
         breakpoint: 520,
@@ -95,7 +94,7 @@ $('.customer-logos').slick({
     }]
 });
 
-$('.page-scroll').on('click', function(e) {
+$('.page-scroll').on('click', function (e) {
     $(this).addClass('active').siblings().removeClass('active');
 
     var tujuan = $(this).attr('href');
